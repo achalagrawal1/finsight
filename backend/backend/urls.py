0cd73ec77a6d9f42184195c1399e62b50fece542
Achal Agrawal <agrawalachal56@gmail.com>
2025-12-25 17:22:36 +0530

    Yes doing it

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,14 +17,15 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path , include
-from .views import home, create_admin
+from .views import home
+# create_admin
 
 
 urlpatterns = [
     path('', home),
     path('admin/', admin.site.urls),
     path('api/', include('expense.urls')),
-    path('create-admin/', create_admin),
+    # path('create-admin/', create_admin),
     
 ]
 
